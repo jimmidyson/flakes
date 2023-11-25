b6a233c5a15c7e85bf594740685594d868ddac9b
Jimmi Dyson <jimmidyson@gmail.com>
2023-11-25 09:42:22 +0000

    fix: Fix up typo in nixpkgs url

--- golangci-lint/flake.nix
+++ golangci-lint/flake.nix
@@ -2,7 +2,7 @@
   description = "golangci-lint built with go 1.21";
 
   inputs = {
-    nixpkgs.url = "gihub:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
