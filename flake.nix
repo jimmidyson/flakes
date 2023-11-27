7d0c3ff6e24a32bbb451ec4a20a3dcecdbdd3b00
Jimmi Dyson <jimmidyson@gmail.com>
2023-11-27 10:55:59 +0000

    refactor: Use inline package in top-level flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Useful flakes for golang and Kubernetes projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = rec {
+        golangci-lint = pkgs.golangci-lint.override { buildGoModule = pkgs.buildGo121Module; };
+      };
+      formatter = pkgs.alejandra;
+    });
+}
